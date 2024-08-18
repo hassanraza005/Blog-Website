0d46addad5a78f17d550dc1cf1df2fa470cc8c1c
Muhammad Hassan Raza <153517064+hassanraza005@users.noreply.github.com>
2024-08-18 11:07:52 +0500

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,55 @@
+
+import {collection,addDoc, getDocs, doc, deleteDoc ,updateDoc } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js'
+import{ db } from "./config.js"
+
+
+
+const dashboardForm = document.querySelector('.dashboard-form')
+const dashboardInput = document.querySelector('.dashboard-input')
+const dashboardTextArea = document.querySelector('.dashboard-textarea')
+const renderBlogs = document.querySelector('.renderBlogs')
+
+let arr = []
+
+ async function getdata(){
+    const querySnapshot = await getDocs(collection(db, "blogs"));
+    querySnapshot.forEach((doc) => {
+    console.log(doc.data());
+});
+
+
+}
+
+
+function renderData(){
+    renderBlogs.innerHTML = ''
+    arr.forEach((item)=>{
+        renderBlogs.innerHTML += `
+        <h2 style ="color:black">${item.title}</h2>
+        <p style ="color:black">${item.content}</p>`
+    })
+}
+
+renderData()
+
+dashboardForm.addEventListener('submit', async (e)=>{
+    e.preventDefault()
+
+    const userValues = {
+        title: dashboardInput.value,
+        content: dashboardTextArea.value,
+    }
+
+    try {
+        const docRef = await addDoc(collection(db, "blogs"), userValues);
+        arr.push(userValues)
+        renderData()
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+
+       dashboardInput.value = ''
+       dashboardTextArea.value = ''
+    
+})
--- login.js
+++ login.js
@@ -1,37 +1,70 @@
-const loginForm = document.querySelector('.login-box form');
-const registerLink = document.querySelector('.login-box p a');
+// const loginForm = document.querySelector('.login-box form');
 
-loginForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+// const registerLink = document.querySelector('.login-box p a');
+
+// loginForm.addEventListener('submit', (event) => {
+//   event.preventDefault();
 
 
-  window.location.href = 'homepage.html'; 
-});
+//   window.location.href = 'homepage.html'; 
+// });
 
-registerLink.addEventListener('click', (event) => {
-  event.preventDefault();
+// registerLink.addEventListener('click', (event) => {
+//   event.preventDefault();
  
-  window.location.href = 'register.html'; 
-});
+//   window.location.href = 'register.html'; 
+// });
 
 
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+// import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-const auth = getAuth();
-const loginForms = document.getElementById('login-form');
-const errorMessage = document.getElementById('error-message');
+// const auth = getAuth();
+// const loginForms = document.getElementById('login-form');
+// const errorMessage = document.getElementById('error-message');
 
-loginForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
+// loginForm.addEventListener('submit', async (event) => {
+//   event.preventDefault();
 
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+//   const email = document.getElementById('email').value;
+//   const password = document.getElementById('password').value;
 
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+//   try {
+//     const userCredential = await signInWithEmailAndPassword(auth, email, password);
   
-    window.location.href = 'homepage.html'; 
-  } catch (error) {
-    errorMessage.textContent = error.message;
-  }
-});
+//     window.location.href = 'homepage.html'; 
+//   } catch (error) {
+//     errorMessage.textContent = error.message;
+//   }
+// });
+
+
+import { signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js"
+import {auth} from "./config.js"
+
+
+
+const loginForm = document.querySelector('#login-form')
+const emailLogin = document.querySelector('.email-login')
+const passwordLogin = document.querySelector('.password-login')
+
+loginForm.addEventListener('submit' , (e)=>{
+    e.preventDefault()
+
+    signInWithEmailAndPassword(auth, emailLogin.value, passwordLogin.value)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user);
+
+    setTimeout(()=>{
+        alert('login succesfully')
+        window.location.href = 'index.html'
+    },1000)
+    
+  })
+  .catch((error) => {
+    const errorMessage = error.message;
+    console.log(errorMessage);
+    
+  });
+})
+
